d436e4521c0094e13d6f9aeef60b3ab80fac72a8
sultzx <sultzx@gmail.com>
2025-06-17 16:40:14 +0500

    cl

--- src/pages/sponsors.jsx
+++ src/pages/sponsors.jsx
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import largeTriangles2 from '../assets/subtle-prism3.svg';
 import { useTranslation } from "react-i18next";
 
+
 export const Sponsors = () => {
     const { t } = useTranslation()
     return (
@@ -11,7 +12,7 @@ export const Sponsors = () => {
                 paddingTop: '0px',
                 paddingBottom: '8rem',
                 backgroundRepeat: 'repeat',
-                height: '100vh',
+                height: '200vh',
                 backgroundSize: 'cover'
                  ,background: `url(${largeTriangles2})`
             }}>
@@ -19,7 +20,9 @@ export const Sponsors = () => {
                 <Row>
                     <Col className="participants-list"  md={12}>
                         <h3 className="mb-4">{t('sponsors.title')}</h3>
-                        <p  style={{margin: '0', fontWeight: '300'}}>{t('sponsors.p1')}</p>
+                        {/* <p  style={{margin: '0', fontWeight: '300'}}>{t('sponsors.p1')}</p> */}
+                        {/* <hr /> */}
+                       <iframe src="/program.pdf" width="100%" height="1500px" style={{border: '1px solid #2196F3', boxShadow: '1px 1px 8px rgba(43, 158, 252, 0.58)'}}/>
                     </Col>
                 </Row>
             </Container>
